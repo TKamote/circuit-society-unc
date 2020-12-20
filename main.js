96b2b597e889f16cfb91d0fa01debef417478210
Verano <kamoteka143din@gmail.com>
2020-12-20 19:04:31 +0800

    1st commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//const title = document.querySelector('h1');
+
+//all JS is for Travesy Media 50/50 below
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+
+})
